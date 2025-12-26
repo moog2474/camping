d53a80375937b735c370725b24f18191d46bf9a4
moog <b.monhoo230@gmail.com>
2025-12-26 14:22:52 +0800

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
                             <a href="#" className="w-10 h-10 bg-slate-800 rounded-full flex items-center justify-center hover:bg-primary transition-colors text-white">
                                 <Instagram className="w-5 h-5" />
                             </a>
-                            <a href="#" className="w-10 h-10 bg-slate-800 rounded-full flex items-center justify-center hover:bg-primary transition-colors text-white">
+                            <a href="https://www.facebook.com/profile.php?id=100057824223298" className="w-10 h-10 bg-slate-800 rounded-full flex items-center justify-center hover:bg-primary transition-colors text-white">
                                 <Facebook className="w-5 h-5" />
                             </a>
                             <a href="#" className="w-10 h-10 bg-slate-800 rounded-full flex items-center justify-center hover:bg-primary transition-colors text-white">
